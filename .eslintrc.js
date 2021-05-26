e0dd1590a17d4330c776bbeee867293357e84911
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-26 14:34:09 +0430

    Added @fullstacksjs to eslint extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["standard", "prettier"],
+  extends: ["standard", "prettier", "@fullstacksjs"],
   parserOptions: {
     ecmaVersion: 12,
   },
