b5d9fbe7274495efa143b50e4e05bcdc99bf6850
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-27 14:31:33 +0430

    Added MrmLintStaged function

--- src/Scripts/Prettier.js
+++ src/Scripts/Prettier.js
@@ -1,6 +1,6 @@
 const path = require("path");
 
-const { AddPackage } = require(path.join(__dirname, "./PackageManager"));
+const { AddPackage, Npx } = require(path.join(__dirname, "./PackageManager"));
 module.exports.InstallPrettier = async (
   PackageManager = global.Config.PackageManager
 ) => {
@@ -8,6 +8,16 @@ module.exports.InstallPrettier = async (
     await AddPackage("prettier", "--dev --exact", PackageManager);
   } catch (err) {
     console.err(err);
+    process.exit();
+  }
+};
+module.exports.MrmLintStaged = async (
+  PackageManager = global.Config.PackageManager
+) => {
+  try {
+    await Npx("mrm lint-staged", "", PackageManager);
+  } catch (err) {
+    console.error(err);
     process.exit();
   }
 };
