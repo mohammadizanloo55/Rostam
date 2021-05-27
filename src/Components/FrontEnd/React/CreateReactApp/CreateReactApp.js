6d0ad613fdb78e93b3c1e8c23287de59c1ad0f53
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-27 14:38:04 +0430

    Added Npx function

--- src/Components/FrontEnd/React/CreateReactApp/CreateReactApp.js
+++ src/Components/FrontEnd/React/CreateReactApp/CreateReactApp.js
@@ -1,20 +1,13 @@
-const { execSync } = require("child_process");
 const path = require("path");
-const ora = require("ora");
 const { DeleteGitFolder, DeleteGitIgnore } = require("../../../../Scripts/Git");
+const { Npx } = require("../../../../Scripts/PackageManager");
 
 const RunCreateReactAppNpx = async () => {
-  const Spinner = ora("Create React App is being installed ...").start();
-
   const { PackageManager, ProjectName, Log } = global.Config;
 
-  const Command = `npx create-react-app ${ProjectName} ${
-    PackageManager === "Yarn" ? "" : "--use-npm"
-  }`;
-  await execSync(Command, {
+  await Npx(`create-react-app ${ProjectName}`, "", PackageManager, {
     stdio: `${Log ? "inherit" : "pipe"}`,
   });
-  Spinner.succeed("Create React App Installed");
 };
 const RunGitRemover = async () => {
   const { UseGit } = global.Config;
