c63f8713e4731c47c905c95cac2ec01be37f3d21
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-27 12:26:19 +0430

    added GitChecker function

--- src/Scripts/Git.js
+++ src/Scripts/Git.js
@@ -1,4 +1,5 @@
 const { rmdirSync, unlinkSync } = require("fs");
+const { execSync } = require("child_process");
 const ora = require("ora");
 
 module.exports.DeleteGitFolder = async () => {
@@ -20,6 +21,16 @@ module.exports.DeleteGitIgnore = async () => {
   } catch (err) {
     Spinner.fail(err);
     console.error(err);
+    process.exit();
+  }
+};
+module.exports.GitChecker = async (
+  message = "You do not have Git on your system, please install it to use it"
+) => {
+  try {
+    await execSync("git --version");
+  } catch (err) {
+    console.error(message);
     process.exit();
   }
 };
