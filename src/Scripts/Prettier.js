67f8aa453e773e049c05263489d0e8dd3e08bed8
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-27 17:55:41 +0430

    Added ChangeLintStagedConfig function

--- src/Scripts/Prettier.js
+++ src/Scripts/Prettier.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const fs = require("fs");
+const ora = require("ora");
 
 const { AddPackage, Npx } = require(path.join(__dirname, "./PackageManager"));
 module.exports.InstallPrettier = async (PackageManager) => {
@@ -14,6 +16,22 @@ module.exports.MrmLintStaged = async (PackageManager) => {
     await Npx("mrm lint-staged", "", PackageManager);
   } catch (err) {
     console.error(err);
+    process.exit();
+  }
+};
+module.exports.ChangeLintStagedConfig = async () => {
+  const Spinner = ora("changing Package.json lint-staged config ... ");
+  try {
+    const PackageDotJson = JSON.parse(await fs.readFileSync("package.json"));
+    PackageDotJson["lint-staged"] = {
+      "*.*": "prettier '**/*' --write --ignore-unknown",
+    };
+    fs.writeFileSync("package.json", JSON.stringify(PackageDotJson, null, 2));
+
+    Spinner.succeed("Package.json lint-staged will be Configured");
+  } catch (err) {
+    Spinner.fail("Package.json lint-staged Could not configure");
+    console.log(err);
     process.exit();
   }
 };
