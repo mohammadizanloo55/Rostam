ed642572c949d88a6cebd18619952ea7fa7c3b82
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-29 10:07:59 +0430

    Added RunCreateGitIgnore function

--- src/Components/BackEnd/Express/Custom/Custom.js
+++ src/Components/BackEnd/Express/Custom/Custom.js
@@ -1,7 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
-const { GitInit } = require(path.join(__dirname, "../../../../Scripts/Git"));
+const { GitInit, CreateGitIgnore, GitAdd, GitCommit } = require(path.join(
+  __dirname,
+  "../../../../Scripts/Git"
+));
 
 const RunGitInit = async () => {
   const { UseGit } = global.Config;
@@ -9,11 +12,25 @@ const RunGitInit = async () => {
     await GitInit();
   }
 };
-
+const RunCreateGitIgnore = async (Path) => {
+  console.log(Path);
+  const { UseGit, GitForConfig } = global.Config;
+  if (UseGit) {
+    const Gitignore = await fs.readFileSync(
+      path.join(__dirname, "GitignoreTemplate.txt")
+    );
+    await CreateGitIgnore(Path, Gitignore);
+    if (GitForConfig) {
+      await GitAdd(".");
+      await GitCommit("Added .gitignore file");
+    }
+  }
+};
 module.exports = async () => {
   const { ProjectName } = global.Config;
   const ProjectDirectory = path.join(process.cwd(), ProjectName);
   await fs.mkdirSync(ProjectDirectory);
   process.chdir(ProjectDirectory);
   await RunGitInit();
+  await RunCreateGitIgnore(ProjectDirectory);
 };
