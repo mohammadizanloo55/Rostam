88d16243121afa9ec7a5c7f96b13d4c44027ad21
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-29 07:48:23 +0430

    added RunGitInit function

--- src/Components/BackEnd/Express/Custom/Custom.js
+++ src/Components/BackEnd/Express/Custom/Custom.js
@@ -1,9 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
+const { GitInit } = require(path.join(__dirname, "../../../../Scripts/Git"));
+
+const RunGitInit = async () => {
+  const { UseGit } = global.Config;
+  if (UseGit) {
+    await GitInit();
+  }
+};
+
 module.exports = async () => {
   const { ProjectName } = global.Config;
   const ProjectDirectory = path.join(process.cwd(), ProjectName);
   await fs.mkdirSync(ProjectDirectory);
   process.chdir(ProjectDirectory);
+  await RunGitInit();
 };
