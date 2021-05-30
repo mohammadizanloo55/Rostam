7c411e11919bb703ac466ece49d53cadf3263aa5
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-05-30 12:10:17 +0430

    Added ProjectDirectoryChecker function

--- src/Scripts/ProjectDirectory.js
+++ src/Scripts/ProjectDirectory.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const ora = require("ora");
+const validate = require("validate-npm-package-name");
 
 module.exports.CreateProjectDirectory = async (Path) => {
   const Spinner = ora("Creating Project Directory...");
@@ -9,6 +10,28 @@ module.exports.CreateProjectDirectory = async (Path) => {
   } catch (error) {
     Spinner.fail("Project Directory not Created");
     console.error();
+    process.exit();
+  }
+};
+module.exports.ProjectDirectoryChecker = async (Name) => {
+  const Spinner = ora("Checking Project Name ...");
+  try {
+    const IsExists = await fs.existsSync(Name);
+    if (IsExists) {
+      throw Error(
+        "Directory with this name is exists remove the Directory or change Project Name"
+      );
+    }
+    if (Name.match(/([A-Z])(.*)/g)) {
+      throw Error("name can no longer contain capital letters");
+    }
+    if (!validate(Name).validForNewPackages) {
+      throw Error(validate(Name).errors);
+    }
+    Spinner.succeed("Project Name is ok");
+  } catch (error) {
+    Spinner.fail("There is a problem with the project name check the log");
+    console.error(error);
     process.exit();
   }
 };
