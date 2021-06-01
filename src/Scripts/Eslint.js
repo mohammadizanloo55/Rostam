73afd3d89a4c21df6f3664f85f64af5038212171
mohammadizanloo55 <mohammadizanloo55@gmail.com>
2021-06-01 14:23:58 +0430

    Added AddedEslintrc function

--- src/Scripts/Eslint.js
+++ src/Scripts/Eslint.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const fs = require("fs");
+const ora = require("ora");
 
 const { AddPackage } = require(path.join(__dirname, "./PackageManager"));
 
@@ -6,6 +8,20 @@ module.exports.InstallEslint = async (PackageManager) => {
   try {
     await AddPackage("eslint", "--dev", PackageManager);
   } catch (error) {
+    console.error(error);
+    process.exit();
+  }
+};
+module.exports.AddedEslintrc = async (Path, Configs) => {
+  const Spinner = ora("Creating eslint Config file").start();
+  try {
+    await fs.writeFileSync(
+      path.join(Path, ".eslintrc.json"),
+      JSON.stringify(Configs, null, 2)
+    );
+    Spinner.succeed("eslint config file Created");
+  } catch (error) {
+    Spinner.fail("eslint config file Could not create");
     console.error(error);
     process.exit();
   }
